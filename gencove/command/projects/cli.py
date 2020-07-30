5d553818e3682a80130ad5216d008f1e95f3e0f4
Mislav Cimperšak <mislav.cimpersak@gencove.com>
2020-07-30 16:46:48 +0200

    adding command to list project's batch types

--- gencove/command/projects/cli.py
+++ gencove/command/projects/cli.py
@@ -3,6 +3,7 @@
 import click
 
 from .list.cli import list_projects
+from .list_batch_types.cli import list_project_batch_types
 from .samples.cli import list_project_samples
 
 
@@ -13,3 +14,4 @@ def projects():
 
 projects.add_command(list_projects)
 projects.add_command(list_project_samples)
+projects.add_command(list_project_batch_types)
---
+++ gencove/command/projects/list_batch_types/main.py
@@ -0,0 +1,80 @@
+"""List project's batch types subcommand."""
+
+import backoff
+
+import requests
+
+# pylint: disable=wrong-import-order
+from gencove.client import APIClientError  # noqa: I100
+from gencove.command.base import Command, ValidationError
+from gencove.command.utils import is_valid_uuid
+
+from .exceptions import BatchTypesError
+from .utils import get_line
+
+
+class ListBatchTypes(Command):
+    """List batch types command executor."""
+
+    def __init__(self, project_id, credentials, options):
+        super(ListBatchTypes, self).__init__(credentials, options)
+
+        self.project_id = project_id
+
+    def initialize(self):
+        """Initialize list subcommand."""
+        self.login()
+
+    def validate(self):
+        """Validate command input.
+
+        Raises:
+            ValidationError - if something is wrong with command parameters.
+        """
+
+        if is_valid_uuid(self.project_id) is False:
+            error_message = "Project ID is not valid. Exiting."
+            self.echo_warning(error_message, err=True)
+            raise ValidationError(error_message)
+
+    def execute(self):
+        self.echo_debug("Retrieving project's batch types:")
+
+        for batch_types in self.get_paginated_batch_types():
+            if not batch_types:
+                self.echo_debug("No matching batch types were found.")
+                return
+
+            for batch_type in batch_types:
+                self.echo(get_line(batch_type))
+
+    def get_paginated_batch_types(self):
+        """Paginate over all batch types for the destination.
+
+        Yields:
+            paginated lists of batch types
+        """
+        more = True
+        next_link = None
+        while more:
+            self.echo_debug("Get batch types page")
+            try:
+                resp = self.get_batch_types(next_link)
+                yield resp["results"]
+                next_link = resp["meta"]["next"]
+                more = next_link is not None
+            except APIClientError as err:
+                self.echo_debug(err)
+                raise BatchTypesError
+
+    @backoff.on_exception(
+        backoff.expo,
+        (requests.exceptions.ConnectionError, requests.exceptions.Timeout),
+        max_tries=2,
+        max_time=30,
+    )
+    def get_batch_types(self, next_link=None):
+        """Get batch types page."""
+        return self.api_client.get_project_batch_types(
+            project_id=self.project_id, next_link=next_link,
+        )

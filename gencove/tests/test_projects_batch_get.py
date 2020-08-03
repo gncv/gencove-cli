51a5451ff89f1186b11d46dab365eb17da10fd7e
Tomaz Berisa <tomaz.berisa@gencove.com>
2020-08-03 18:49:31 -0400

    Add test for existing batch

--- gencove/tests/test_projects_batch_get.py
+++ gencove/tests/test_projects_batch_get.py
@@ -13,7 +13,7 @@ def test_get_batch__empty(mocker):
     runner = CliRunner()
     mocked_login = mocker.patch.object(APIClient, "login", return_value=None)
     batch_id = str(uuid4())
-    mocked_get_project_batches = mocker.patch.object(
+    mocked_get_batch = mocker.patch.object(
         APIClient,
         "get_batch",
         return_value=dict(
@@ -35,7 +35,7 @@ def test_get_batch__empty(mocker):
     )
     assert res.exit_code == 1
     mocked_login.assert_called_once()
-    mocked_get_project_batches.assert_called_once()
+    mocked_get_batch.assert_called_once()
     assert (
         res.output
         == "There are no deliverables available for batch {}.\n".format(
@@ -44,68 +44,54 @@ def test_get_batch__empty(mocker):
     )
 
 
-# MOCKED_BATCHES = dict(
-#     meta=dict(count=2, next=None),
-#     results=[
-#         {
-#             "id": str(uuid4()),
-#             "name": "foo",
-#             "batch_type": "hd777k",
-#             "files": [
-#                 {
-#                     "id": str(uuid4()),
-#                     "file_type": "report-zip",
-#                     "download_url": "https://foo.com/bar.zip",
-#                 }
-#             ],
-#         },
-#         {
-#             "id": str(uuid4()),
-#             "name": "bar",
-#             "batch_type": "hd777k",
-#             "files": [
-#                 {
-#                     "id": str(uuid4()),
-#                     "file_type": "report-zip",
-#                     "download_url": "https://baz.com/bar.zip",
-#                 }
-#             ],
-#         },
-#     ],
-# )
-
-
-# def test_get_batch__not_empty(mocker):
-#     """Test project batches being outputed to the shell."""
-#     runner = CliRunner()
-#     mocked_login = mocker.patch.object(APIClient, "login", return_value=None)
-#     mocked_get_project_batches = mocker.patch.object(
-#         APIClient, "get_project_batches", return_value=MOCKED_BATCHES
-#     )
-
-#     res = runner.invoke(
-#         get_batch,
-#         [str(uuid4()), "--email", "foo@bar.com", "--password", "123"],
-#     )
-#     assert res.exit_code == 0
-#     mocked_login.assert_called_once()
-#     mocked_get_project_batches.assert_called_once()
-
-#     output_line = io.BytesIO()
-#     sys.stdout = output_line
-#     line_one = "\t".join(
-#         [
-#             MOCKED_BATCHES["results"][0]["id"],
-#             MOCKED_BATCHES["results"][0]["batch_type"],
-#             MOCKED_BATCHES["results"][0]["name"],
-#         ]
-#     )
-#     line_two = "\t".join(
-#         [
-#             MOCKED_BATCHES["results"][1]["id"],
-#             MOCKED_BATCHES["results"][1]["batch_type"],
-#             MOCKED_BATCHES["results"][1]["name"],
-#         ]
-#     )
-#     echo("\n".join([line_one, line_two]))
-#     assert output_line.getvalue() == res.output.encode()
+def test_get_batch__not_empty(mocker):
+    """Test project batches being outputed to the shell."""
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        mocked_login = mocker.patch.object(
+            APIClient, "login", return_value=None
+        )
+        batch_id = str(uuid4())
+        mocked_get_batch = mocker.patch.object(
+            APIClient,
+            "get_batch",
+            return_value=dict(
+                id=batch_id,
+                name="cli-test-1",
+                batch_type="batch-type-1",
+                sample_ids=["sample-id-1", "sample-id-2"],
+                last_status=dict(
+                    id="last-status-id-1",
+                    status="running",
+                    created="2020-08-02T22:13:54.547167Z",
+                ),
+                files=[
+                    dict(
+                        id="file-id-1",
+                        s3_path="output/simple_vcf2finalreport/job-id-1/simple_vcf2finalreport/report.zip",
+                        size=None,
+                        download_url="https://bucket.s3.amazonaws.com/output/simple_vcf2finalreport/job-id-1/simple_vcf2finalreport/report.zip",
+                        file_type="report-zip",
+                    )
+                ],
+            ),
+        )
+        mocked_download_file = mocker.patch(
+            "gencove.command.projects.get_batch.main.download.utils.download_file"
+        )
+        res = runner.invoke(
+            get_batch,
+            [
+                batch_id,
+                "--email",
+                "foo@bar.com",
+                "--password",
+                "123",
+                "--output-filename",
+                "test.zip",
+            ],
+        )
+    assert res.exit_code == 0
+    mocked_login.assert_called_once()
+    mocked_get_batch.assert_called_once()
+    mocked_download_file.assert_called_once()

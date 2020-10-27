23b8585e611d75f54b310807d91a6ad58cc751fd
Zlatko Masek <zlatko.masek@gencove.com>
2020-10-27 18:17:14 +0000

    Reorder super call in ValidationError exception.

--- gencove/exceptions.py
+++ gencove/exceptions.py
@@ -5,5 +5,5 @@ class ValidationError(Exception):
     """CLI validation error - indicating command input is not valid."""
 
     def __init__(self, message=""):
+        super().__init__(message)
         self.message = message
-        super().__init__(self.message)

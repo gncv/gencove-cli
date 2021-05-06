5087844b2ca3b6dfced38b64e1a49e079c85742a
Miguel Liezun <liezun.js@gmail.com>
2021-05-06 15:26:58 -0300

    fix broken pipe error & add examples

--- gencove/command/samples/download_file/cli.py
+++ gencove/command/samples/download_file/cli.py
@@ -10,7 +10,7 @@ from .main import DownloadFile
 @click.command("download-file")
 @click.argument("sample_id")
 @click.argument("file_type")
-@click.argument("destination", type=click.File("wb"))
+@click.argument("destination")
 @add_options(common_options)
 @click.option(
     "--no-progress",
@@ -27,13 +27,39 @@ def download_file(
     password,
     api_key,
     no_progress,
-):
-    """Download sample file metadata."""
-    DownloadFile(
-        sample_id,
-        file_type,
-        destination,
-        Credentials(email, password, api_key),
-        Optionals(host),
-        no_progress,
-    ).run()
+):  # noqa: D413,D301,D412 # pylint: disable=C0301
+    """Download sample file metadata.
+
+    Must specify SAMPLE_ID, FILE_TYPE and DESTINATION.
+
+    Examples:
+
+        Download sample:
+
+            gencove samples download-file e6b45af7-07c5-4a6d-9f97-6e1efbf3e215 ancestry-json ancestry.json
+
+        Download and print to stdout then compress using gzip:
+
+            gencove samples download-file e6b45af7-07c5-4a6d-9f97-6e1efbf3e215 ancestry-json - | gzip > ancestry.json.gz
+    \f
+
+    Args:
+        sample_id (str): specific samples for which
+            to download the results.
+        file_type (str): specific deliverable to download
+            results for.
+        destination (str): path/to/file.
+        no_progress (bool, optional, default False): do not show progress
+            bar.
+    """  # noqa: E501
+    if destination == "-":
+        destination = "/dev/stdout"
+    with open(destination, "wb") as destination_file:
+        DownloadFile(
+            sample_id,
+            file_type,
+            destination_file,
+            Credentials(email, password, api_key),
+            Optionals(host),
+            no_progress,
+        ).run()

493cb8e3ef08a9cb2b8f4496f78f528466d70015
Miguel Liezun <liezun.js@gmail.com>
2021-05-25 17:36:43 -0300

    making models attributes optional

--- gencove/command/projects/list/constants.py
+++ gencove/command/projects/list/constants.py
@@ -1,4 +1,6 @@
 """Describe constants in projects subcommand."""
+from typing import Optional
+
 from gencove.models import GencoveBaseModel
 
 
@@ -6,30 +8,30 @@ from gencove.models import GencoveBaseModel
 class Project(GencoveBaseModel):
     """Project record"""
 
-    name: str
-    description: str
-    created: str
-    organization: str
-    sample_count: str
-    pipeline_capabilities: str
+    name: Optional[str]
+    description: Optional[str]
+    created: Optional[str]
+    organization: Optional[str]
+    sample_count: Optional[str]
+    pipeline_capabilities: Optional[str]
 
 
 # pylint: disable=too-few-public-methods
 class PipelineCapabilities(GencoveBaseModel):
     """Pipeline Capabilities record"""
 
-    name: str
-    private: str
-    merge_vcfs_enabled: str
+    name: Optional[str]
+    private: Optional[str]
+    merge_vcfs_enabled: Optional[str]
 
 
 # pylint: disable=too-few-public-methods
 class ProjectWithPipelineCapabilities(GencoveBaseModel):
     """Project record with PipelineCapabilities"""
 
-    name: str
-    description: str
-    created: str
-    organization: str
-    sample_count: str
-    pipeline_capabilities: PipelineCapabilities
+    name: Optional[str]
+    description: Optional[str]
+    created: Optional[str]
+    organization: Optional[str]
+    sample_count: Optional[str]
+    pipeline_capabilities: Optional[PipelineCapabilities]

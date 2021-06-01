a863ee7d9d711ec90ed039c0aebe2705581e7ccb
Miguel Liezun <liezun.js@gmail.com>
2021-06-01 14:46:44 -0300

    Complete models for api endpoints

--- gencove/models.py
+++ gencove/models.py
@@ -1,6 +1,6 @@
 """Gencove CLI models"""
 from datetime import datetime
-from typing import List, Optional, Union
+from typing import Any, List, Optional, Union
 from uuid import UUID
 
 from pydantic import BaseModel, HttpUrl  # pylint: disable=no-name-in-module
@@ -37,12 +37,13 @@ class S3Object(BaseModel):
     object_name: str
 
 
-class UploadStatusObject(GencoveBaseModel):
-    """UploadStatusObject model"""
+class StatusObject(GencoveBaseModel):
+    """StatusObject model"""
 
     status: str
-    note: str
-    created: str
+    note: Optional[str]
+    created: datetime
+    transition_cutoff: Optional[datetime]
 
 
 class UploadsPostDataResponse(GencoveBaseModel):
@@ -50,7 +51,7 @@ class UploadsPostDataResponse(GencoveBaseModel):
 
     destination_path: str
     s3: S3Object
-    last_status: UploadStatusObject
+    last_status: StatusObject
 
 
 class ResponseMeta(BaseModel):
@@ -66,8 +67,8 @@ class PipelineCapabilities(GencoveBaseModel):
     """Pipeline Capabilities record"""
 
     name: Optional[str]
-    private: Optional[str]
-    merge_vcfs_enabled: Optional[str]
+    private: Optional[bool]
+    merge_vcfs_enabled: Optional[bool]
 
 
 # pylint: disable=too-few-public-methods
@@ -80,11 +81,175 @@ class Project(GencoveBaseModel):
     organization: Optional[str]
     sample_count: Optional[int]
     pipeline_capabilities: Optional[Union[UUID, PipelineCapabilities]]
+    roles: Optional[dict]
     webhook_url: Optional[HttpUrl]  # deprecated
+
+
+class UploadCredentialsResponse(BaseModel):
+    """UploadCredentialsResponse model"""
+
+    version: int
+    access_key: str
+    secret_key: str
+    token: str
+    expiry_time: datetime
+
+
+class SampleFile(GencoveBaseModel):
+    """SampleFile model"""
+
+    s3_path: str
+    size: Optional[int]
+    download_url: HttpUrl
+    file_type: str
+
+
+class SampleDetails(GencoveBaseModel):
+    """SampleDetails model"""
+
+    created: datetime
+    modified: datetime
+    client_id: str
+    physical_id: str
+    legacy_id: str
+    last_status: StatusObject
+    archive_last_status: StatusObject
+    files: Optional[List[SampleFile]]
 
 
 class GetProjectSamplesResponse(BaseModel):
     """GetProjectSamplesResponse model"""
 
     meta: ResponseMeta
-    result: Optional[List[Project]]
+    results: Optional[List[SampleDetails]]
+
+
+class UploadCreate(BaseModel):
+    """UploadCreate model"""
+
+    upload: UUID
+
+
+class Fastqs(BaseModel):
+    """Fastqs model"""
+
+    r1: UploadCreate
+    r2: UploadCreate
+
+
+class SampleSheetCreate(BaseModel):
+    """SampleSheetCreate model"""
+
+    client_id: UUID
+    fastq: Fastqs
+
+
+class CreateProjectSamplesResponse(BaseModel):
+    """CreateProjectSamplesResponse model"""
+
+    uploads: List[SampleSheetCreate]
+    metadata: Optional[Any]
+
+
+class QualityControlType(BaseModel):
+    """QualityControlType model"""
+
+    key: str
+    type: str
+
+
+class QualityControlSelf(BaseModel):
+    """QualityControlSelf model"""
+
+    value_expected: Optional[float]
+    value_measured: Optional[float]
+    status: str
+
+
+class QualityControl(BaseModel):
+    """QualityControl model"""
+
+    quality_control_type: QualityControlType
+    quality_control: QualityControlSelf
+
+
+class SampleQCResponse(BaseModel):
+    """SampleQCResponse model"""
+
+    meta: ResponseMeta
+    results: List[QualityControl]
+
+
+class UploadNestedList(BaseModel):
+    """UploadNestedList model"""
+
+    upload: UUID
+    destination_path: Optional[str]
+    last_status: StatusObject
+
+
+class UploadFastQ(BaseModel):
+    """UploadFastQ model"""
+
+    client_id: UUID
+    fastq: UploadNestedList
+
+
+class SampleSheetResponse(BaseModel):
+    """SampleSheetResponse model"""
+
+    meta: ResponseMeta
+    results: Optional[List[UploadFastQ]]
+
+
+class GetProjectsResponse(BaseModel):
+    """GetProjectsResponse model"""
+
+    meta: ResponseMeta
+    results: Optional[List[Project]]
+
+
+class BatchType(BaseModel):
+    """BatchType model"""
+
+    key: str
+    description: str
+
+
+class ProjectBatchTypesResponse(BaseModel):
+    """ProjectBatchTypesResponse model"""
+
+    meta: ResponseMeta
+    results: Optional[List[BatchType]]
+
+
+class BatchDetail(GencoveBaseModel):
+    """BatchDetail model"""
+
+    name: str
+    batch_type: str
+    sample_ids: Optional[List[UUID]]
+    last_status: StatusObject
+    files: List[SampleFile]
+
+
+class GetProjectBatchesResponse(BaseModel):
+    """GetProjectBatchesResponse model"""
+
+    meta: ResponseMeta
+    results: Optional[List[BatchDetail]]
+
+
+class ProjectMergeVCFs(GencoveBaseModel):
+    """ProjectMergeVCFs model"""
+
+    created: datetime
+    user: UUID
+    last_status: StatusObject
+    up_to_date: bool
+
+
+class SampleMetadata(GencoveBaseModel):
+    """SampleMetadata model"""
+
+    metadata: Optional[Any]

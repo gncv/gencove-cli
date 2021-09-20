a2c83afc081d39eff3d024a6bb3b95a7fa59c3b4
Cristian Mora <cristian.mora@gencove.com>
2021-09-20 16:13:06 -0300

    Use VCR on test_upload_and_run_immediately_with_output_to_file

--- gencove/tests/upload/test_cli_upload.py
+++ gencove/tests/upload/test_cli_upload.py
@@ -54,6 +54,9 @@ def vcr_config():
             ("email", "email@example.com"),
             ("password", "mock_password"),
         ],
+        "filter_query_parameters": [
+            ("search", "gncv://cli-mock/test.fastq.gz")
+        ],
         "match_on": ["method", "scheme", "port", "path", "query"],
         "path_transformer": VCR.ensure_suffix(".yaml"),
         "before_record_request": [
@@ -264,9 +267,7 @@ def test_upload_and_run_immediately(
         mocked_regular_progress_bar.assert_called_once()
 
 
-@pytest.mark.vcr(
-    filter_query_parameters=[("search", "gncv://cli-mock/test.fastq.gz")],
-)
+@pytest.mark.vcr
 @assert_authorization
 def test_upload_and_run_immediately__with_metadata(
     credentials, mocker, project_id, recording, vcr
@@ -487,95 +488,92 @@ def test_upload_and_run_immediately_something_went_wrong(mocker):
         mocked_assign_sample.assert_not_called()
 
 
-def test_upload_and_run_immediately_with_output_to_file(mocker):
+@pytest.mark.vcr
+@assert_authorization
+def test_upload_and_run_immediately_with_output_to_file(
+    credentials, mocker, project_id, recording, vcr
+):
     """Upload and assign right away, then save results to a file."""
+    # pylint: disable=too-many-locals
     runner = CliRunner()
     with runner.isolated_filesystem():
         os.mkdir("cli_test_data")
         with open("cli_test_data/test.fastq.gz", "w") as fastq_file:
             fastq_file.write("AAABBB")
 
-        mocked_login = mocker.patch.object(
-            APIClient, "login", return_value=None
-        )
         mocked_get_credentials = mocker.patch(
-            "gencove.command.upload.main.get_s3_client_refreshable"
-        )
-        upload_id = str(uuid4())
-        mocked_get_upload_details = mocker.patch.object(
-            APIClient,
-            "get_upload_details",
-            return_value=UploadsPostData(
-                **{
-                    "id": upload_id,
-                    "last_status": {"id": str(uuid4()), "status": ""},
-                    "s3": {"bucket": "test", "object_name": "test"},
-                }
-            ),
+            "gencove.command.upload.main.get_s3_client_refreshable",
+            side_effect=get_s3_client_refreshable,
         )
         mocked_upload_file = mocker.patch(
-            "gencove.command.upload.main.upload_file"
-        )
-        mocked_get_sample_sheet = mocker.patch.object(
-            APIClient,
-            "get_sample_sheet",
-            return_value=SampleSheet(
-                **{
-                    "meta": {"next": None},
-                    "results": [
-                        {
-                            "client_id": "foo",
-                            "fastq": {"r1": {"upload": upload_id}},
-                        }
-                    ],
-                }
-            ),
-        )
-        sample_id = str(uuid4())
-        mocked_response = {
-            "uploads": [
-                {
-                    "client_id": "foo",
-                    "fastq": {"r1": {"upload": upload_id}},
-                    "sample": sample_id,
-                }
-            ],
-        }
-        mocked_assign_sample = mocker.patch.object(
-            APIClient,
-            "add_samples_to_project",
-            return_value=UploadSamples(**mocked_response),
+            "gencove.command.upload.main.upload_file", side_effect=upload_file
         )
 
-        mocker.patch("gencove.command.upload.main.get_regular_progress_bar")
+        mocked_regular_progress_bar = mocker.patch(
+            "gencove.command.upload.main.get_regular_progress_bar",
+            side_effect=get_regular_progress_bar,
+        )
+
+        if not recording:
+            # Mock get_upload credentials only if using the cassettes, since
+            # we mock the return value.
+            upload_details_response = get_vcr_response(
+                "/api/v2/uploads-post-data/", vcr
+            )
+            mocked_get_upload_details = mocker.patch.object(
+                APIClient,
+                "get_upload_details",
+                return_value=UploadsPostData(**upload_details_response),
+            )
+            sample_sheet_response = get_vcr_response(
+                "/api/v2/sample-sheet/", vcr
+            )
+            mocked_get_sample_sheet = mocker.patch.object(
+                APIClient,
+                "get_sample_sheet",
+                return_value=SampleSheet(**sample_sheet_response),
+            )
+            project_sample_response = get_vcr_response(
+                "/api/v2/project-samples/", vcr, operator.contains
+            )
+            mocked_assign_sample = mocker.patch.object(
+                APIClient,
+                "add_samples_to_project",
+                return_value=UploadSamples(**project_sample_response),
+            )
         res = runner.invoke(
             upload,
             [
                 "cli_test_data",
-                "--email",
-                "foo@bar.com",
-                "--password",
-                "123456",
+                *credentials,
                 "--run-project-id",
-                "11111111-1111-1111-1111-111111111111",
+                project_id,
                 "--output",
                 "samples.json",
             ],
         )
 
+        assert not res.exception
         assert res.exit_code == 0
-        mocked_login.assert_called_once()
         mocked_get_credentials.assert_called_once()
-        mocked_get_upload_details.assert_called_once()
         mocked_upload_file.assert_called_once()
-        mocked_get_sample_sheet.assert_called()
-        mocked_assign_sample.assert_called_once()
-        output_content = None
-        with open("samples.json", "r") as output_file:
-            output_content = output_file.read()
-        assert (
-            json.dumps(mocked_response["uploads"], indent=4) == output_content
-        )
+        mocked_regular_progress_bar.assert_called_once()
+
+        if not recording:
+            mocked_get_upload_details.assert_called_once()
+            mocked_get_sample_sheet.assert_called()
+            mocked_assign_sample.assert_called_once()
+            with open("samples.json", "r") as output_file:
+                output_content = output_file.read()
+            # r2 fastq key is not present on the file
+            assert (
+                project_sample_response["uploads"][0]["fastq"]["r2"] is None
+            )
+            del project_sample_response["uploads"][0]["fastq"]["r2"]
+            assert (
+                json.dumps(project_sample_response["uploads"], indent=4)
+                == output_content
+            )
 
 
 def test_upload_and_run_immediately_with_output_to_nested_file(mocker):

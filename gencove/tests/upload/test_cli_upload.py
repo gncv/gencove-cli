0ec709850cf3c5e99d5d9aec3d140526f58f8f5a
Cristian Mora <cristian.mora@gencove.com>
2021-09-21 15:05:23 -0300

    Use VCR on test_upload_and_run_immediately_with_stdout

--- gencove/tests/upload/test_cli_upload.py
+++ gencove/tests/upload/test_cli_upload.py
@@ -2,14 +2,11 @@
 
 # pylint: disable=too-many-lines, import-error
 
-import io
 import json
 import operator
 import os
-import sys
 from uuid import uuid4
 
-from click import echo
 from click.testing import CliRunner
 
 
@@ -661,94 +658,90 @@ def test_upload_and_run_immediately_with_output_to_nested_file(
             )
 
 
-def test_upload_and_run_immediately_with_stdout(mocker):
+@pytest.mark.vcr
+@assert_authorization
+def test_upload_and_run_immediately_with_stdout(
+    credentials, mocker, project_id, recording, vcr
+):
     """Upload and assign right away, then print out the results."""
+    # pylint: disable=too-many-locals
     runner = CliRunner()
     with runner.isolated_filesystem():
         os.mkdir("cli_test_data")
         with open("cli_test_data/test.fastq.gz", "w") as fastq_file:
             fastq_file.write("AAABBB")
 
-        mocked_login = mocker.patch.object(
-            APIClient, "login", return_value=None
-        )
         mocked_get_credentials = mocker.patch(
-            "gencove.command.upload.main.get_s3_client_refreshable"
-        )
-        upload_id = str(uuid4())
-        mocked_get_upload_details = mocker.patch.object(
-            APIClient,
-            "get_upload_details",
-            return_value=UploadsPostData(
-                **{
-                    "id": upload_id,
-                    "last_status": {"id": str(uuid4()), "status": ""},
-                    "s3": {"bucket": "test", "object_name": "test"},
-                }
-            ),
-        )
-        mocked_upload_file = mocker.patch(
-            "gencove.command.upload.main.upload_file"
-        )
-        mocked_get_sample_sheet = mocker.patch.object(
-            APIClient,
-            "get_sample_sheet",
-            return_value=SampleSheet(
-                **{
-                    "meta": {"next": None},
-                    "results": [
-                        {
-                            "client_id": "foo",
-                            "fastq": {"r1": {"upload": upload_id}},
-                        }
-                    ],
-                }
-            ),
-        )
-        sample_id = str(uuid4())
-        mocked_response = {
-            "uploads": [
-                {
-                    "client_id": "foo",
-                    "fastq": {"r1": {"upload": upload_id}},
-                    "sample": sample_id,
-                }
-            ],
-        }
-        mocked_assign_sample = mocker.patch.object(
-            APIClient,
-            "add_samples_to_project",
-            return_value=UploadSamples(**mocked_response),
+            "gencove.command.upload.main.get_s3_client_refreshable",
+            side_effect=get_s3_client_refreshable,
         )
 
-        mocker.patch("gencove.command.upload.main.get_regular_progress_bar")
+        if not recording:
+            # Mock get_upload credentials only if using the cassettes, since
+            # we mock the return value.
+            upload_details_response = get_vcr_response(
+                "/api/v2/uploads-post-data/", vcr
+            )
+            mocked_get_upload_details = mocker.patch.object(
+                APIClient,
+                "get_upload_details",
+                return_value=UploadsPostData(**upload_details_response),
+            )
+            sample_sheet_response = get_vcr_response(
+                "/api/v2/sample-sheet/", vcr
+            )
+            mocked_get_sample_sheet = mocker.patch.object(
+                APIClient,
+                "get_sample_sheet",
+                return_value=SampleSheet(**sample_sheet_response),
+            )
+            project_sample_response = get_vcr_response(
+                "/api/v2/project-samples/", vcr, operator.contains
+            )
+            mocked_assign_sample = mocker.patch.object(
+                APIClient,
+                "add_samples_to_project",
+                return_value=UploadSamples(**project_sample_response),
+            )
+            # These mocks needs to be here and without side effect otherwise
+            # a weird behavior on the stdout will happend and the test fails.
+            mocked_upload_file = mocker.patch(
+                "gencove.command.upload.main.upload_file"
+            )
+
+            mocker.patch(
+                "gencove.command.upload.main.get_regular_progress_bar",
+            )
 
         res = runner.invoke(
             upload,
             [
                 "cli_test_data",
-                "--email",
-                "foo@bar.com",
-                "--password",
-                "123456",
+                *credentials,
                 "--run-project-id",
-                "11111111-1111-1111-1111-111111111111",
+                project_id,
                 "--output",
                 "-",
             ],
         )
 
         assert res.exit_code == 0
-        mocked_login.assert_called_once()
+
         mocked_get_credentials.assert_called_once()
-        mocked_get_upload_details.assert_called_once()
-        mocked_upload_file.assert_called_once()
-        mocked_get_sample_sheet.assert_called()
-        mocked_assign_sample.assert_called_once()
-        output_line = io.BytesIO()
-        sys.stdout = output_line
-        echo(json.dumps(mocked_response["uploads"], indent=4))
-        assert output_line.getvalue() in res.output.encode()
+        if not recording:
+            mocked_upload_file.assert_called_once()
+            mocked_get_upload_details.assert_called_once()
+            mocked_get_sample_sheet.assert_called()
+            mocked_assign_sample.assert_called_once()
+            # r2 fastq key is not present on the file
+            assert (
+                project_sample_response["uploads"][0]["fastq"]["r2"] is None
+            )
+            del project_sample_response["uploads"][0]["fastq"]["r2"]
+            assert (
+                json.dumps(project_sample_response["uploads"], indent=4)
+                in res.output
+            )
 
 
 def test_upload_without_progressbar(mocker):

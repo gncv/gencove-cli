0baaea254a18eb26628562bc5d8597cf3d6356f6
Cristian Mora <cristian.mora@gencove.com>
2021-09-29 11:49:44 -0300

    Use VCR on multiple uploads tests
    Use VCR on:
    * test_list_does_not_exist
    * test_list_empty
    * test_list_uploads_slow_response_retry

--- gencove/tests/uploads/test_uploads_list.py
+++ gencove/tests/uploads/test_uploads_list.py
@@ -1,5 +1,5 @@
 """Test uploads list command."""
-# pylint: disable=wrong-import-order
+# pylint: disable=wrong-import-order, import-error
 import io
 import sys
 from uuid import uuid4
@@ -10,12 +10,51 @@ from click.testing import CliRunner
 from gencove.client import APIClient, APIClientError, APIClientTimeout
 from gencove.command.uploads.list.cli import list_uploads
 from gencove.models import SampleSheet
+from gencove.tests.decorators import assert_authorization
+from gencove.tests.filters import filter_jwt, replace_gencove_url_vcr
+from gencove.tests.upload.vcr.filters import filter_volatile_dates
+
+import pytest
+
+from vcr import VCR
 
 
-def test_list_does_not_exist(mocker):
+@pytest.fixture(scope="module")
+def vcr_config():
+    """VCR configuration."""
+    return {
+        "cassette_library_dir": "gencove/tests/uploads/vcr",
+        "filter_headers": [
+            "Authorization",
+            "Content-Length",
+            "User-Agent",
+            # "X-Amz-Security-Token",
+            # "X-Amz-Date",
+        ],
+        "filter_post_data_parameters": [
+            ("email", "email@example.com"),
+            ("password", "mock_password"),
+        ],
+        "filter_query_parameters": [
+            # ("search", "gncv://cli-mock/test.fastq.gz")
+        ],
+        "match_on": ["method", "scheme", "port", "path", "query"],
+        "path_transformer": VCR.ensure_suffix(".yaml"),
+        "before_record_request": [
+            replace_gencove_url_vcr,
+        ],
+        "before_record_response": [
+            filter_jwt,
+            filter_volatile_dates,
+        ],
+    }
+
+
+@pytest.mark.vcr
+@assert_authorization
+def test_list_does_not_exist(mocker, credentials):
     """Test user cannot get to uploads."""
     runner = CliRunner()
-    mocked_login = mocker.patch.object(APIClient, "login", return_value=None)
     mocked_get_projects = mocker.patch.object(
         APIClient,
         "get_sample_sheet",
@@ -24,11 +63,9 @@ def test_list_does_not_exist(mocker):
         ),
         return_value={"detail": "Not found"},
     )
-    res = runner.invoke(
-        list_uploads, ["--email", "foo@bar.com", "--password", "123"]
-    )
+    res = runner.invoke(list_uploads, credentials)
+    assert isinstance(res.exception, SystemExit)
     assert res.exit_code == 1
-    mocked_login.assert_called_once()
     mocked_get_projects.assert_called_once()
     assert (
         "\n".join(
@@ -42,20 +79,18 @@ def test_list_does_not_exist(mocker):
     )
 
 
-def test_list_empty(mocker):
+@pytest.mark.vcr
+@assert_authorization
+def test_list_empty(mocker, credentials):
     """Test user has no uploads."""
     runner = CliRunner()
-    mocked_login = mocker.patch.object(APIClient, "login", return_value=None)
     mocked_get_projects = mocker.patch.object(
         APIClient,
         "get_sample_sheet",
         return_value=SampleSheet(results=[], meta=dict(next=None)),
     )
-    res = runner.invoke(
-        list_uploads, ["--email", "foo@bar.com", "--password", "123"]
-    )
+    res = runner.invoke(list_uploads, credentials)
     assert res.exit_code == 0
-    mocked_login.assert_called_once()
     mocked_get_projects.assert_called_once()
     assert "" in res.output
 
@@ -89,51 +124,22 @@ MOCKED_UPLOADS = dict(
 )
 
 
-def test_list_uploads_slow_response_retry(mocker):
+@pytest.mark.vcr
+@assert_authorization
+def test_list_uploads_slow_response_retry(mocker, credentials):
     """Test list uploads slow response retry."""
     runner = CliRunner()
-    mocked_login = mocker.patch.object(APIClient, "login", return_value=None)
     mocked_get_sample_sheet = mocker.patch.object(
         APIClient,
         "get_sample_sheet",
         side_effect=APIClientTimeout("Could not connect to the api server"),
     )
-    res = runner.invoke(
-        list_uploads, ["--email", "foo@bar.com", "--password", "123"]
-    )
+    res = runner.invoke(list_uploads, credentials)
     assert res.exit_code == 1
-    mocked_login.assert_called_once()
     assert mocked_get_sample_sheet.call_count == 5
-
-    output_line = io.BytesIO()
-    sys.stdout = output_line
-    echo(
-        "\t".join(
-            [
-                MOCKED_UPLOADS["results"][0]["client_id"],
-                MOCKED_UPLOADS["results"][0]["fastq"]["r1"]["upload"],
-                MOCKED_UPLOADS["results"][0]["fastq"]["r1"][
-                    "destination_path"
-                ],
-                MOCKED_UPLOADS["results"][0]["fastq"]["r2"]["upload"],
-                MOCKED_UPLOADS["results"][0]["fastq"]["r2"][
-                    "destination_path"
-                ],
-            ]
-        )
+    assert (
+        res.output == "ERROR: Could not connect to the api server\nAborted!\n"
     )
-    echo(
-        "\t".join(
-            [
-                MOCKED_UPLOADS["results"][1]["client_id"],
-                MOCKED_UPLOADS["results"][1]["fastq"]["r1"]["upload"],
-                MOCKED_UPLOADS["results"][1]["fastq"]["r1"][
-                    "destination_path"
-                ],
-            ]
-        )
-    )
-    assert output_line.getvalue() != res.output.encode()
 
 
 def test_list_uploads(mocker):

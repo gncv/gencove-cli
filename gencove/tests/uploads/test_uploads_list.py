78e7906797e0f8a2c1eabb01f1a0ba3a5f77e41c
Cristian Mora <cristian.mora@gencove.com>
2021-09-29 12:25:46 -0300

    Use VCR on test_list_uploads

--- gencove/tests/uploads/test_uploads_list.py
+++ gencove/tests/uploads/test_uploads_list.py
@@ -1,10 +1,6 @@
 """Test uploads list command."""
 # pylint: disable=wrong-import-order, import-error
-import io
-import sys
-from uuid import uuid4
 
-from click import echo
 from click.testing import CliRunner
 
 from gencove.client import APIClient, APIClientError, APIClientTimeout
@@ -12,7 +8,11 @@ from gencove.command.uploads.list.cli import list_uploads
 from gencove.models import SampleSheet
 from gencove.tests.decorators import assert_authorization
 from gencove.tests.filters import filter_jwt, replace_gencove_url_vcr
-from gencove.tests.upload.vcr.filters import filter_volatile_dates
+from gencove.tests.upload.vcr.filters import (
+    filter_sample_sheet_response,
+    filter_volatile_dates,
+)
+from gencove.tests.utils import get_vcr_response
 
 import pytest
 
@@ -28,15 +28,10 @@ def vcr_config():
             "Authorization",
             "Content-Length",
             "User-Agent",
-            # "X-Amz-Security-Token",
-            # "X-Amz-Date",
         ],
         "filter_post_data_parameters": [
             ("email", "email@example.com"),
             ("password", "mock_password"),
-        ],
-        "filter_query_parameters": [
-            # ("search", "gncv://cli-mock/test.fastq.gz")
         ],
         "match_on": ["method", "scheme", "port", "path", "query"],
         "path_transformer": VCR.ensure_suffix(".yaml"),
@@ -46,6 +41,7 @@ def vcr_config():
         "before_record_response": [
             filter_jwt,
             filter_volatile_dates,
+            filter_sample_sheet_response,
         ],
     }
 
@@ -95,35 +91,6 @@ def test_list_empty(mocker, credentials):
     assert "" in res.output
 
 
-MOCKED_UPLOADS = dict(
-    meta=dict(next=None),
-    results=[
-        {
-            "client_id": "clientid1",
-            "fastq": {
-                "r1": {
-                    "upload": str(uuid4()),
-                    "destination_path": "gncv://batch1/clientid1_R1.fastq.gz",
-                },
-                "r2": {
-                    "upload": str(uuid4()),
-                    "destination_path": "gncv://batch1/clientid1_R2.fastq.gz",
-                },
-            },
-        },
-        {
-            "client_id": "clientid2",
-            "fastq": {
-                "r1": {
-                    "upload": str(uuid4()),
-                    "destination_path": "gncv://batch2/clientid2_R1.fastq.gz",
-                }
-            },
-        },
-    ],
-)
-
-
 @pytest.mark.vcr
 @assert_authorization
 def test_list_uploads_slow_response_retry(mocker, credentials):
@@ -142,48 +109,34 @@ def test_list_uploads_slow_response_retry(mocker, credentials):
     )
 
 
-def test_list_uploads(mocker):
+@pytest.mark.vcr
+@assert_authorization
+def test_list_uploads(mocker, credentials, recording, vcr):
     """Test list uploads being outputed to the shell."""
     runner = CliRunner()
-    mocked_login = mocker.patch.object(APIClient, "login", return_value=None)
-    mocked_get_sample_sheet = mocker.patch.object(
-        APIClient,
-        "get_sample_sheet",
-        return_value=SampleSheet(**MOCKED_UPLOADS),
-    )
-    res = runner.invoke(
-        list_uploads, ["--email", "foo@bar.com", "--password", "123"]
-    )
+    if not recording:
+        sample_sheet_response = get_vcr_response("/api/v2/sample-sheet/", vcr)
+        mocked_get_sample_sheet = mocker.patch.object(
+            APIClient,
+            "get_sample_sheet",
+            return_value=SampleSheet(**sample_sheet_response),
+        )
+    res = runner.invoke(list_uploads, credentials)
     assert res.exit_code == 0
-    mocked_login.assert_called_once()
-    mocked_get_sample_sheet.assert_called_once()
 
-    output_line = io.BytesIO()
-    sys.stdout = output_line
-    echo(
-        "\t".join(
+    if not recording:
+        mocked_get_sample_sheet.assert_called()
+        uploads = sample_sheet_response["results"]
+        uploads = "\n".join(
             [
-                MOCKED_UPLOADS["results"][0]["client_id"],
-                MOCKED_UPLOADS["results"][0]["fastq"]["r1"]["upload"],
-                MOCKED_UPLOADS["results"][0]["fastq"]["r1"][
-                    "destination_path"
-                ],
-                MOCKED_UPLOADS["results"][0]["fastq"]["r2"]["upload"],
-                MOCKED_UPLOADS["results"][0]["fastq"]["r2"][
-                    "destination_path"
-                ],
+                "\t".join(
+                    [
+                        upload["client_id"],
+                        upload["fastq"]["r1"]["upload"],
+                        upload["fastq"]["r1"]["destination_path"],
+                    ]
+                )
+                for upload in uploads
             ]
         )
-    )
-    echo(
-        "\t".join(
-            [
-                MOCKED_UPLOADS["results"][1]["client_id"],
-                MOCKED_UPLOADS["results"][1]["fastq"]["r1"]["upload"],
-                MOCKED_UPLOADS["results"][1]["fastq"]["r1"][
-                    "destination_path"
-                ],
-            ]
-        )
-    )
-    assert output_line.getvalue() == res.output.encode()
+        assert f"{uploads}\n" == res.output

98806628838ba7956cbaf88e1c631f6af92c9846
Cristian Mora <cristian.mora@gencove.com>
2021-09-30 17:10:44 -0300

    Use VCR on test_projects_list.py tests

---
+++ gencove/tests/projects/vcr/filters.py
@@ -0,0 +1,50 @@
+"""VCR filters for projects tests."""
+
+import copy
+
+from gencove.tests.decorators import parse_response_to_json
+from gencove.tests.utils import MOCK_UUID
+
+
+@parse_response_to_json
+def filter_list_projects_response(response, json_response):
+    """Filter list project sensitive data from response."""
+    if "results" in json_response:
+        for result in json_response["results"]:
+            if "id" in result:
+                result["id"] = MOCK_UUID
+            if "name" in result:
+                result["name"] = "mock name"
+            if "description" in result and result["description"]:
+                result["description"] = "mock description"
+            if "organization" in result:
+                result["organization"] = MOCK_UUID
+            if "sample_count" in result:
+                result["sample_count"] = 1
+            if "pipeline_capabilities" in result:
+                result["pipeline_capabilities"] = MOCK_UUID
+            if "roles" in result:
+                result["roles"]["organization"]["id"] = MOCK_UUID
+            if "webhook_url" in result and result["webhook_url"]:
+                result["webhook_url"] = "http://mockurl.com"
+    return response, json_response
+
+
+def filter_pipeline_capabilities_request(request):
+    """Filter pipeline capabilities sensitive data from request."""
+    request = copy.deepcopy(request)
+    if "pipeline-capabilities" in request.path:
+        # remove the project id from the url
+        base_uri = request.uri.split("pipeline-capabilities")[0]
+        request.uri = base_uri + f"pipeline-capabilities/{MOCK_UUID}"
+    return request
+
+
+@parse_response_to_json
+def filter_pipeline_capabilities_response(response, json_response):
+    """Filter pipeline capabilities sensitive data from response."""
+    if "id" in json_response:
+        json_response["id"] = MOCK_UUID
+    if "name" in json_response:
+        json_response["name"] = "mock name"
+    return response, json_response

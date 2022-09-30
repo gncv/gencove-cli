1a98abc5ce843726bee1336bd8580ff8552ca6e0
nicolas.markos <nicolas.markos@gencove.com>
2022-09-30 14:06:09 +0300

    reformat file

--- gencove/tests/projects/test_projects_samples_list.py
+++ gencove/tests/projects/test_projects_samples_list.py
@@ -8,6 +8,7 @@ from uuid import uuid4
 from click.testing import CliRunner
 
 from gencove.client import APIClient, APIClientError, APIClientTimeout
+from gencove.command.projects.samples.utils import get_line
 from gencove.command.projects.cli import list_project_samples
 from gencove.logger import echo_data
 from gencove.models import ProjectSamples, SampleDetails
@@ -22,6 +23,31 @@ from gencove.tests.utils import get_vcr_response
 import pytest
 
 from vcr import VCR
+
+
+@pytest.fixture(scope="module")
+def sample_archive_status_null():
+    """A sample containing a NULL (eg. deleted) archive status"""
+    return {
+        "meta": {"count": 23, "next": None, "previous": None},
+        "results": [
+            {
+                "id": "11111111-1111-1111-1111-111111111111",
+                "created": "2021-09-21T18:30:44.799519Z",
+                "modified": "2021-09-21T18:38:57.735776Z",
+                "client_id": "mock client_id",
+                "physical_id": "",
+                "legacy_id": "",
+                "last_status": {
+                    "id": "11111111-1111-1111-1111-111111111111",
+                    "status": "mock status",
+                    "note": "",
+                    "created": "2021-09-21T18:38:57.735776Z",
+                },
+                "archive_last_status": None,
+            }
+        ],
+    }
 
 
 @pytest.fixture(scope="module")
@@ -198,3 +224,33 @@ def test_list_project_samples(
                 )
             )
         assert output_line.getvalue() == res.output.encode()
+
+
+@assert_authorization
+def test_list_project_samples__archive_status_null__prints_without_fail(
+    mocker, credentials, project_id, sample_archive_status_null
+):  # pylint: disable=unused-argument
+    """Test project samples being outputed to the shell."""
+    runner = CliRunner()
+
+    # Mock get_project_samples only if using the cassettes,
+    # since we mock the return value.
+    mocked_sample__archive_status_null = sample_archive_status_null
+    mocked_get_project_samples = mocker.patch.object(
+        APIClient,
+        "get_project_samples",
+        return_value=ProjectSamples(**mocked_sample__archive_status_null),
+    )
+    res = runner.invoke(
+        list_project_samples,
+        [project_id, *credentials],
+    )
+    assert res.exit_code == 0
+    mocked_get_project_samples.assert_called_once()
+    output_line = io.BytesIO()
+    sys.stdout = output_line
+    for mocked_sample in mocked_sample__archive_status_null["results"]:
+        mocked_sample = SampleDetails(**mocked_sample)
+
+        echo_data(get_line(mocked_sample))
+    assert output_line.getvalue() == res.output.encode()

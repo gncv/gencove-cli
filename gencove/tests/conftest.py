f4b9af8d19e3feb3825f32c0c5c6f4cdc8b2aea5
nicolas.markos <nicolas.markos@gencove.com>
2022-10-04 02:27:37 +0300

    Move fixture to conftest

--- gencove/tests/conftest.py
+++ gencove/tests/conftest.py
@@ -126,3 +126,28 @@ def dump_filename(mocker):
         side_effect=get_debug_file_name_patch,
     )
     return filename
+
+
+@pytest.fixture(scope="function")
+def sample_archive_status_null():
+    """A sample containing a NULL (eg. deleted) archive status"""
+    return {
+        "meta": {"count": 1, "next": None, "previous": None},
+        "results": [
+            {
+                "id": "11111111-1111-1111-1111-111111111111",
+                "created": "2021-09-21T18:30:44.799519Z",
+                "modified": "2021-09-21T18:38:57.735776Z",
+                "client_id": "mock client_id",
+                "physical_id": "",
+                "legacy_id": "",
+                "last_status": {
+                    "id": "11111111-1111-1111-1111-111111111111",
+                    "status": "mock status",
+                    "note": "",
+                    "created": "2021-09-21T18:38:57.735776Z",
+                },
+                "archive_last_status": None,
+            }
+        ],
+    }

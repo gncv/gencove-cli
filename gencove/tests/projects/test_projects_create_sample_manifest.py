180a489d4d0ce391e36bc39aa159e2326755fec3
Forest Dussault <forest.dussault@gencove.com>
2023-08-28 10:54:58 -0400

    Add create sample manifest success test

---
+++ gencove/tests/projects/test_projects_create_sample_manifest.py
@@ -0,0 +1,121 @@
+"""Test project's create sample manifest command."""
+import csv
+
+# pylint: disable=wrong-import-order, import-error
+import io
+import operator
+import sys
+import tempfile
+import uuid
+
+from click import echo
+from click.testing import CliRunner
+
+from gencove.client import APIClient, APIClientError  # noqa: I100
+from gencove.command.projects.cli import create_sample_manifest
+from gencove.command.utils import sanitize_string
+from gencove.models import Project, SampleManifest
+from gencove.tests.decorators import assert_authorization
+from gencove.tests.filters import filter_jwt, replace_gencove_url_vcr
+from gencove.tests.projects.vcr.filters import (
+    filter_create_project_request,
+    filter_create_project_response,
+    filter_project_sample_manifest_request,
+)
+from gencove.tests.upload.vcr.filters import filter_volatile_dates
+from gencove.tests.utils import get_vcr_response
+
+import pytest
+
+from vcr import VCR
+
+
+@pytest.fixture(scope="module")
+def vcr_config():
+    """VCR configuration."""
+    return {
+        "cassette_library_dir": "gencove/tests/projects/vcr",
+        "filter_headers": [
+            "Authorization",
+            "Content-Length",
+            "User-Agent",
+        ],
+        "filter_post_data_parameters": [
+            ("email", "email@example.com"),
+            ("password", "mock_password"),
+        ],
+        "match_on": ["method", "scheme", "port", "path", "query"],
+        "path_transformer": VCR.ensure_suffix(".yaml"),
+        "before_record_request": [
+            replace_gencove_url_vcr,
+            filter_project_sample_manifest_request,
+        ],
+        "before_record_response": [
+            filter_jwt,
+            filter_volatile_dates,
+        ],
+    }
+
+
+@pytest.fixture(scope="function")
+def dummy_valid_manifest_csv():
+    """Valid manifest CSV file"""
+
+    manifest_columns = [
+        "Plate ID",
+        "Well",
+        "Unique sample identifier",
+        "Buffer",
+        "Concentration if known (ng/ul)",
+        "Species",
+        "Coverage",
+    ]
+
+    well_pattern_96 = [
+        letter + str(num) for num in range(1, 13) for letter in "ABCDEFGH"
+    ]
+    with tempfile.NamedTemporaryFile(
+        mode="w+", suffix=".csv", delete=False
+    ) as tmp_file:
+        writer = csv.writer(tmp_file)
+        writer.writerow(manifest_columns)  # header
+        writer.writerow(["", "A1", f"client-id", "", "", "", ""])
+        for well in well_pattern_96[1:]:
+            writer.writerow(["", well, f"", "", "", "", ""])
+        tmp_file.seek(0)
+    return tmp_file.name
+
+
+@pytest.mark.vcr
+@assert_authorization
+def test_create_sample_manifest__success(
+    credentials,
+    mocker,
+    project_id_sample_manifest,
+    dummy_valid_manifest_csv,
+    recording,
+    vcr,
+):
+    runner = CliRunner()
+    if not recording:
+        # Mock create_project only if using the cassettes, since we
+        # mock the return value.
+        create_sample_manifest_response = get_vcr_response(
+            "/api/v2/project-sample-manifests/", vcr, operator.contains
+        )
+        mocked_create_sample_manifest = mocker.patch.object(
+            APIClient,
+            "create_sample_manifest",
+            return_value={},
+        )
+    res = runner.invoke(
+        create_sample_manifest,
+        [
+            project_id_sample_manifest,
+            dummy_valid_manifest_csv,
+            *credentials,
+        ],
+    )
+    assert res.exit_code == 0
+    if not recording:
+        mocked_create_sample_manifest.assert_called_once()

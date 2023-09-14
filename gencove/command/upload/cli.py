270ba93592af0f7502126238873122fba15d7dd6
Mislav Cimperšak <mislav.cimpersak@gencove.com>
2023-09-14 10:36:41 +0200

    removed all ignore instructions from docstrings

--- gencove/command/upload/cli.py
+++ gencove/command/upload/cli.py
@@ -21,7 +21,10 @@ from .main import Upload
     "--output",
     required=False,
     default=None,
-    help="A destination where to store the resulting assignments.",
+    help=(
+        "A destination where to store the resulting assignments. "
+        "Only compatible with --run-project-id."
+    ),
 )
 @click.option(
     "--no-progress",
@@ -33,7 +36,7 @@ from .main import Upload
     required=False,
     default=None,
     help=(
-        "Assign metadata to all samples created from uploads. "
+        "Assign JSON metadata to all samples created from uploads. "
         "Only compatible with --run-project-id."
     ),
 )
@@ -51,11 +54,11 @@ def upload(  # pylint: disable=E0012,C0330,R0913
 ):  # noqa: D301
     """Upload FASTQ files to Gencove's system.
 
-    SOURCE: folder that contains fastq files to be uploaded (acceptable file
-    extensions are .fastq.gz, .fastq.bgz, .fq.gz, .fq.bgz), or .fastq-map.csv
+    `SOURCE`: folder that contains fastq files to be uploaded (acceptable file
+    extensions are .fastq.gz, .fastq.bgz, .fq.gz, .fq.bgz), OR .fastq-map.csv
     file with the mapping of R1/R2 files to related sample by client_id
 
-    DESTINATION (optional): gncv://[folder], where the folder is the location
+    `DESTINATION` (optional): `gncv://[folder]`, where the folder is the location
     on Gencove systems
 
     Examples:
@@ -63,24 +66,6 @@ def upload(  # pylint: disable=E0012,C0330,R0913
         Upload directory contents:
 
             gencove upload test_dataset gncv://test
-
-    \f
-    .. ignore::
-        Args:
-            source (.fastq.gz, .fastq.bgz, .fq.gz, .fq.bgz):
-                folder that contains fastq files to be uploaded
-                OR .fastq-map.csv file
-            destination (str, optional): 'gncv://' notated folder
-                on Gencove's system, where the files will be uploaded to.
-            run_project_id (UUID, optional): ID of a project to which all files
-                in this upload will be assigned to and then immediately analyzed.
-            output (str, optional): must be used with run_project_id. "-"
-                redirects the JSON to STDOUT, and a name redirects the output to
-                a file.
-            no_progress (bool, optional, default False): do not show progress
-                bar.
-            metadata (str, optional): JSON metadata to be applied to all samples
-
     """
     Upload(
         source,

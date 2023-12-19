749745ee8fc30ef8d756e486ee7e550f47ab073f
Forest Dussault <forest.dussault@gencove.com>
2023-12-19 11:33:57 -0500

    flake8/pylint fixes

--- gencove/tests/explorer/test_data_common.py
+++ gencove/tests/explorer/test_data_common.py
@@ -1,12 +1,23 @@
+"""Tests for command.explorer.data.common"""
 from unittest.mock import patch
 
+# pylint: disable=wrong-import-order, import-error
+
 import pytest
-from gencove.command.explorer.data.common import GencoveExplorerManager, AWSCredentials
+
+from gencove.command.explorer.data.common import (  # noqa: I100
+    AWSCredentials,
+    GencoveExplorerManager,
+)
 
 
-class TestGencoveExplorerManager:
+class TestGencoveExplorerManager:  # pylint: disable=too-many-public-methods
+    """Test GencoveExplorerManager"""
+
     @pytest.fixture(autouse=True)
     def setup(self):
+        """Initialize mocked instance of GencoveExplorerManager"""
+        # pylint: disable=attribute-defined-outside-init
         self.user_id = "11111111-1111-1111-1111-111111111111"
         self.organization_id = "11111111-1111-1111-1111-111111111111"
         self.org_id_short = self.organization_id.replace("-", "")[:12]
@@ -20,73 +31,91 @@ class TestGencoveExplorerManager:
         )
 
     def test_bucket_name(self):
+        """Test bucket_name"""
         assert (
-            self.explorer_manager.bucket_name == f"gencove-explorer-{self.org_id_short}"
+            self.explorer_manager.bucket_name
+            == f"gencove-explorer-{self.org_id_short}"  # noqa: E501 pylint: disable=line-too-long
         )
 
     def test_users_prefix(self):
+        """Test users_prefix"""
         assert self.explorer_manager.users_prefix == "users"
 
     def test_base_prefix(self):
+        """Test base_prefix"""
         expected_prefix = f"users/{self.user_id}"
         assert self.explorer_manager.base_prefix == expected_prefix
 
     def test_shared_prefix(self):
+        """Test shared_prefix"""
         assert self.explorer_manager.shared_prefix == "shared"
 
     def test_data_org_prefix(self):
+        """Test data_org_prefix"""
         expected_prefix = "shared/files"
         assert self.explorer_manager.data_org_prefix == expected_prefix
 
     def test_data_prefix(self):
+        """Test data_prefix"""
         expected_prefix = f"users/{self.user_id}/files"
         assert self.explorer_manager.data_prefix == expected_prefix
 
     def test_user_prefix(self):
+        """Test user_prefix"""
         expected_prefix = f"users/{self.user_id}/files"
         assert self.explorer_manager.user_prefix == expected_prefix
 
     def test_tmp_prefix(self):
+        """Test tmp_prefix"""
         expected_prefix = f"users/{self.user_id}/files/tmp"
         assert self.explorer_manager.tmp_prefix == expected_prefix
 
     def test_tmp_org_prefix(self):
+        """Test tmp_org_prefix"""
         expected_prefix = "shared/files/tmp"
         assert self.explorer_manager.tmp_org_prefix == expected_prefix
 
     def test_scratch_prefix(self):
+        """Test scratch_prefix"""
         expected_prefix = f"users/{self.user_id}/scratch"
         assert self.explorer_manager.scratch_prefix == expected_prefix
 
     def test_user_scratch_s3_prefix(self):
+        """Test user_scratch_s3_prefix"""
         expected_prefix = (
             f"s3://gencove-explorer-{self.org_id_short}/users/{self.user_id}/scratch"
         )
         assert self.explorer_manager.user_scratch_s3_prefix == expected_prefix
 
     def test_shared_org_s3_prefix(self):
+        """Test shared_org_s3_prefix"""
         expected_prefix = f"s3://gencove-explorer-{self.org_id_short}/shared"
         assert self.explorer_manager.shared_org_s3_prefix == expected_prefix
 
     def test_data_gencove_s3_prefix(self):
+        """Test data_gencove_s3_prefix"""
         expected_prefix = "s3://gencove-explorer-data/files"
         assert self.explorer_manager.data_gencove_s3_prefix == expected_prefix
 
     def test_data_org_s3_prefix(self):
+        """Test data_org_s3_prefix"""
         expected_prefix = f"s3://gencove-explorer-{self.org_id_short}/shared/files"
         assert self.explorer_manager.data_org_s3_prefix == expected_prefix
 
     def test_data_s3_prefix(self):
+        """Test data_s3_prefix"""
         expected_prefix = (
             f"s3://gencove-explorer-{self.org_id_short}/users/{self.user_id}/files"
         )
         assert self.explorer_manager.data_s3_prefix == expected_prefix
 
     def test_users_s3_prefix(self):
+        """Test users_s3_prefix"""
         expected_prefix = f"s3://gencove-explorer-{self.org_id_short}/users"
         assert self.explorer_manager.users_s3_prefix == expected_prefix
 
     def test_aws_env(self):
+        """Test aws_env"""
         expected = {
             "AWS_ACCESS_KEY_ID": "mock_access",
             "AWS_SECRET_ACCESS_KEY": "mock_secret",
@@ -104,30 +133,34 @@ class TestGencoveExplorerManager:
         ],
     )
     def test_uri_ok(self, path, expected):
+        """Test uri_ok()"""
         assert self.explorer_manager.uri_ok(path) == expected
 
     def test_translate_path_to_s3_path_valid(self):
+        """Test translate_path_to_s3_path"""
         translated_path = self.explorer_manager.translate_path_to_s3_path(
             path="e://users/me"
         )
         assert (
             translated_path
-            == "s3://gencove-explorer-111111111111/users/11111111-1111-1111-1111-111111111111/files/"
+            == f"s3://gencove-explorer-{self.org_id_short}/users/{self.organization_id}/files/"  # noqa: E501 pylint: disable=line-too-long
         )  # noqa: E501
 
     def test_translate_path_to_s3_path_invalid(self):
-        # Test with an invalid e:// path and expect a ValueError
+        """Test with an invalid e:// path and expect a ValueError"""
         with pytest.raises(ValueError):
             self.explorer_manager.translate_path_to_s3_path("e://invalid/path")
 
     @patch("gencove.command.explorer.data.common.GencoveExplorerManager.list_users")
     def test_list_users(self, mock_list_users):
+        """Test list_users()"""
         mock_list_users.return_value = "user1\nuser2"
         self.explorer_manager.list_users()
         mock_list_users.assert_called_once()
 
     @patch("gencove.command.explorer.data.common.sh.aws", create=True)
-    def test_execute_aws_s3_src_dst(self, mocked_aws):
+    def test_execute_aws_s3_src_dst(self, mocked_aws):  # pylint: disable= W0613
+        """Test execute_aws_s3_src_dst()"""
         cmd = "cp"
         source = "e://users/me/source"
         destination = "e://users/me/destination"
@@ -145,7 +178,10 @@ class TestGencoveExplorerManager:
         assert s3_cmd == [cmd, translated_source, translated_destination, *args]
 
     @patch("gencove.command.explorer.data.common.sh.aws", create=True)
-    def test_execute_aws_s3_src_dst_invalid_path(self, mocked_aws):
+    def test_execute_aws_s3_src_dst_invalid_path(
+        self, mocked_aws
+    ):  # noqa: E501 pylint: disable=W0613
+        """Test execute_aws_s3_src_dst() with invalid src and dst"""
         cmd = "cp"
         source = "invalid"
         destination = "invalid"
@@ -155,7 +191,8 @@ class TestGencoveExplorerManager:
             self.explorer_manager.execute_aws_s3_src_dst(cmd, source, destination, args)
 
     @patch("gencove.command.explorer.data.common.sh.aws", create=True)
-    def test_execute_execute_aws_s3_path(self, mocked_aws):
+    def test_execute_execute_aws_s3_path(self, mocked_aws):  # pylint: disable=W0613
+        """Test execute_aws_s3_path()"""
         cmd = "cp"
         path = "e://users/me/source"
         args = ["--arg1", "--arg2"]
@@ -167,7 +204,10 @@ class TestGencoveExplorerManager:
         assert s3_cmd == [cmd, translated_path, *args]
 
     @patch("gencove.command.explorer.data.common.sh.aws", create=True)
-    def test_execute_execute_aws_s3_path_invalid_path(self, mocked_aws):
+    def test_execute_execute_aws_s3_path_invalid_path(
+        self, mocked_aws
+    ):  # noqa: E501 pylint: disable=W0613
+        """Test execute_aws_s3_path() with invalid path"""
         cmd = "cp"
         path = "invalid"
         args = ["--arg1", "--arg2"]

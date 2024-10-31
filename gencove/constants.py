f81cfb50764eafdd1928ece5e517a859d7dbd5d4
Max Rozenblum <maximillian.rozenblum@gencove.com>
2024-10-31 20:02:23 +0000

    revert HOST back to prod

--- gencove/constants.py
+++ gencove/constants.py
@@ -4,7 +4,7 @@ from typing import Optional
 
 from pydantic import BaseModel  # pylint: disable=no-name-in-module
 
-HOST = "https://api-dev.gencove-dev.com"
+HOST = "https://dev.gencove.com"
 
 
 @unique

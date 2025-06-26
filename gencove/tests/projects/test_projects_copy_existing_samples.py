1ff031f4322e1e3f02a66d8b0c25f7a53a08401a
Miguel Liezun <miguel.liezun@gencove.com>
2025-06-26 10:28:46 +0200

    fix import errors

--- gencove/tests/projects/test_projects_copy_existing_samples.py
+++ gencove/tests/projects/test_projects_copy_existing_samples.py
@@ -12,16 +12,15 @@ from gencove.command.projects.cli import copy_existing_project_samples
 from gencove.constants import IMPORT_BATCH_SIZE
 from gencove.models import (
     CopyExistingSamplesModel,
-    ProjectSamples,
     SampleCopy,
 )
 from gencove.tests.decorators import assert_authorization, assert_no_requests
 from gencove.tests.filters import filter_jwt, replace_gencove_url_vcr
 from gencove.tests.projects.vcr.filters import (
-    filter_get_project_samples_request,
-    filter_get_project_samples_response,
     filter_copy_existing_samples_request,
     filter_copy_existing_samples_response,
+    filter_get_project_samples_request,
+    filter_get_project_samples_response,
 )
 from gencove.tests.utils import MOCK_UUID, get_vcr_response
 from gencove.utils import batchify

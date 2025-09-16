4a56faaf48947e24f8fbf5e6568a354f180bb862
Forest Dussault <forest.dussault@gencove.com>
2025-09-16 16:51:47 -0400

    pylint fix

--- gencove/version.py
+++ gencove/version.py
@@ -5,12 +5,18 @@ import os
 def version():
     """Return version string."""
     base_dir = os.path.dirname(__file__)
-    with open(os.path.join(base_dir, "version", "A-major")) as f:
-        major = f.read().strip()
-    with open(os.path.join(base_dir, "version", "B-minor")) as f:
-        minor = f.read().strip()
-    with open(os.path.join(base_dir, "version", "C-patch")) as f:
-        patch = f.read().strip()
+    with open(
+        os.path.join(base_dir, "version", "A-major"), encoding="utf-8"
+    ) as major_file:
+        major = major_file.read().strip()
+    with open(
+        os.path.join(base_dir, "version", "B-minor"), encoding="utf-8"
+    ) as minor_file:
+        minor = minor_file.read().strip()
+    with open(
+        os.path.join(base_dir, "version", "C-patch"), encoding="utf-8"
+    ) as patch_file:
+        patch = patch_file.read().strip()
     return f"{major}.{minor}.{patch}"
 
 

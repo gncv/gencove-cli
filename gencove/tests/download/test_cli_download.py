41f3d6593237d96ff895b39779a8d62f4743ec3c
Forest Dussault <forest.dussault@gencove.com>
2025-10-15 13:01:19 -0400

    Add CI debugging line

--- gencove/tests/download/test_cli_download.py
+++ gencove/tests/download/test_cli_download.py
@@ -1209,6 +1209,7 @@ def test_parallel_download(credentials, mocker, recording, sample_id_download, v
                 *credentials,
             ],
         )
+        print(res.stdout)
         assert res.exit_code == 0
         if not recording:
             mocked_sample_details.assert_called_once()
